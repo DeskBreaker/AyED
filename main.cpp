bb837402a0523dd6d9daab136d316af18b20299c
Nicolas Gomez <ngomezgonzalez@frba.utn.edu.ar>
2025-05-05 13:10:23 -0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+using namespace std; 
+int main()
+{
+    string nombre = "Nicolas";
+    string apellido = "Gomez Gonzalez";
+    cout << "Hola " << nombre << " "<< apellido;
+    return 0;
+}
 
